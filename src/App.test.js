39a1c3dcf1f8e5e1c9509a7ee2eba2e372edf697
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-24 13:54:28 -0500

    created tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,6 @@ import App from "./App";
 import { shallow } from "enzyme";
 import PersonList from "./PersonList";
 
-// it("renders learn react link", () => {
-//     render( < App / > );
-//     const linkElement = screen.getByText(/learn react/i);
-//     expect(linkElement).toBeInTheDocument();
-// });
-
 describe("App", () => {
     let appWrapper;
     beforeAll(() => {
