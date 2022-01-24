c190def546d61e523ec152061b4232f2f0be4b7e
KateM3d <ekaterina8medvedeva@gmail.com>
2022-01-24 13:52:22 -0500

    created class component App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+import PersonList from "./PersonList";
+import "./App.css";
+
+class App extends Component {
+  state = { people: [] };
+  render() {
+    return (
+      <div className="App">
+        <PersonList people={this.state.people} />
+      </div>
+    );
+  }
+}
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { shallow } from "enzyme";
+import PersonList from "./PersonList";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// it("renders learn react link", () => {
+//     render( < App / > );
+//     const linkElement = screen.getByText(/learn react/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App", () => {
+    let appWrapper;
+    beforeAll(() => {
+        appWrapper = shallow( < App / > );
+    });
+
+    it("renders a person list", () => {
+        //to display some sort of components that gives the list of names
+        const personList = appWrapper.find(PersonList);
+
+        expect(personList).toHaveLength(1);
+    });
+
+    it("has state", () => {
+        const appState = appWrapper.state();
+
+        expect(appState).not.toBeNull();
+    });
+
+    it("has a people property on state", () => {
+        const appState = appWrapper.state();
+
+        expect(appState.people).toBeDefined();
+    });
+
+    it("passes people property of state to personList as prop", () => {
+        const personList = appWrapper.find(PersonList);
+        expect(personList.props().people).toEqual(appWrapper.state().people);
+    });
 });
