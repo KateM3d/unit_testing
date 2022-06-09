852fa32f1e27a10b687371ac3ff4b36063cec450
KateM3d <ekaterina8medvedeva@gmail.com>
2022-06-09 19:14:53 -0400

    add checkbox test

--- color-button/src/App.test.jsx
+++ color-button/src/App.test.jsx
@@ -5,13 +5,21 @@ test("button has correct initial color", () => {
   render(<App />);
   //find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
-
   //expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
   //click button
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
-
   //expect the button text to be 'Change to red'
   expect(colorButton.textContent).toBe("Change to red");
+});
+
+test("initial condition", () => {
+  render(<App />);
+  //check that the button starts out enabled
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
 });
